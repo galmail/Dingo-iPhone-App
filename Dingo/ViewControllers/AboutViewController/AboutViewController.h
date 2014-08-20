504bb1725561e59d92f9d56e4e070395c10837e4
Tigran Aslanyan <admin@Admins-Mac.local>
2014-08-20 19:23:05 +0400

    About

---
+++ Dingo/ViewControllers/AboutViewController/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Dingo
+//
+//  Created by Tigran Aslanyan on 20.08.14.
+//  Copyright (c) 2014 Dingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ Dingo/ViewControllers/AboutViewController/AboutViewController.m
@@ -0,0 +1,42 @@
+//
+//  AboutViewController.m
+//  Dingo
+//
+//  Created by Tigran Aslanyan on 20.08.14.
+//  Copyright (c) 2014 Dingo. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)back {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
