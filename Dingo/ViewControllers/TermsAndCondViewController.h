43a9d1b9eda5c766293cf5341de40caf70a7daea
Tigran Aslanyan <admin@Admins-Mac.local>
2014-09-02 15:12:40 +0400

    PrivacyPolicy And Terms&Cond scenes

---
+++ Dingo/ViewControllers/TermsAndCondViewController.h
@@ -0,0 +1,13 @@
+//
+//  TermsAndCondViewController.h
+//  Dingo
+//
+//  Created by Tigran Aslanyan on 02.09.14.
+//  Copyright (c) 2014 Dingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TermsAndCondViewController : UIViewController
+
+@end
